cad9006576ebbfd8cc6926bd5dbe6b1363126eca
Yash Pujara <67305071+yashapujara@users.noreply.github.com>
2023-07-15 14:00:34 +0530

    Add files via upload

---
+++ Assignment_2/database.php
@@ -0,0 +1,13 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="test";
+
+$conn= mysqli_connect($servername,$username,$password,$dbname);
+
+    // if(!$conn){
+    //     echo "failed";
+    // }
+
+    // echo "success";
